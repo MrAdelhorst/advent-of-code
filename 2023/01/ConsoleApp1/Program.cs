60457084660b1584187efb73b698c861b757f33f
Kenneth A. Andersen <kean@demant.com>
2023-12-01 12:38:24 +0100

    Solution for day 1

---
+++ 2023/01/ConsoleApp1/Program.cs
@@ -0,0 +1,50 @@
+﻿bool IsDigit(char c) => c >= '0' && c <= '9';
+(char number, int index) FindTextNumber(string line, bool reverse = false)
+{
+    var numbers = new[]
+    {
+        new {text = "one", num = '1'},
+        new {text = "two", num = '2'},
+        new {text = "three", num = '3'},
+        new {text = "four", num = '4'},
+        new {text = "five", num = '5'},
+        new {text = "six", num = '6'},
+        new {text = "seven", num = '7'},
+        new {text = "eight", num = '8'},
+        new {text = "nine", num = '9'}
+    };
+
+    var startIdx = reverse ? line.Length - 1 : 0;
+    var finalIdx = reverse ? 0 : line.Length - 1;
+    var delta = reverse ? -1 : 1;
+
+    for (int i = startIdx; i != finalIdx; i += delta)
+    {
+        var val = numbers.FirstOrDefault(num => line.Substring(i).StartsWith(num.text));
+        if (val != null)
+            return (val.num, i);
+    }
+
+    return ('0', -1);
+}
+
+var sum = 0;
+foreach (var line in File.ReadAllLines(@"..\..\..\..\day-01.txt"))
+{
+    //First digit
+    var firstDigit = line.First(IsDigit);
+    var firstDigitIndex = line.IndexOf(firstDigit);
+    var firstTextNumber = FindTextNumber(line);
+    var first = firstTextNumber.number != '0' ? (firstTextNumber.index < firstDigitIndex ? firstTextNumber.number : firstDigit) : firstDigit;
+
+    //Last digit
+    var lastDigit = line.Last(IsDigit);
+    var lastDigitIndex = line.LastIndexOf(lastDigit);
+    var lastTextNumber = FindTextNumber(line, true);
+    var last = lastTextNumber.number != '0' ? (lastTextNumber.index > lastDigitIndex ? lastTextNumber.number : lastDigit) : lastDigit;
+    sum += int.Parse(new string(new[] { first, last }));
+}
+
+//Print result
+Console.WriteLine($"Calibration sum: {sum}");
+Console.ReadLine();
