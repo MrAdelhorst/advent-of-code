3300b73c543f7a253c10fa55a716ac74ba08b349
Kenneth A. Andersen <kean@demant.com>
2023-12-08 21:00:32 +0100

    Renamed folders

---
+++ 2022/01/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        internal static int[] highest = new int[3];
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines(@"c:\data\aoc\1\aoc1-1.txt");
+            var current = 0;
+            foreach (var line in lines)
+            {
+                if (int.TryParse(line, out var cal))
+                    current += cal;
+                else
+                {
+                    StoreIfHigher(current);
+                    current = 0;
+                }
+            }
+            StoreIfHigher(current);
+            Console.WriteLine($"Highest calorie counts: {highest[0]}, {highest[1]}, {highest[2]}, total: {highest[0]+ highest[1]+ highest[2]}");
+            Console.ReadLine();
+        }
+
+        internal static void StoreIfHigher(int current)
+        {
+            int temp = 0;
+            if (current > highest[0])
+            {
+                temp = highest[0];
+                highest[0] = current;
+                current = temp;
+            }
+            if (current > highest[1])
+            {
+                temp = highest[1];
+                highest[1] = current;
+                current = temp;
+            }
+            highest[2] = Math.Max(highest[2], current);
+        }
+    }
+}
---
+++ 2022/06/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentIdx = -1;
+            string currentString = string.Empty;
+            var input = File.ReadAllText(@"c:\data\aoc\6\aoc6-1.txt");
+            do
+            {
+                currentIdx++;
+                currentString = input.Substring(currentIdx, 14);
+            } while (currentString.Distinct().Count() != 14);
+
+            Console.Write($"Result: {currentIdx + 14}");
+            Console.ReadLine();
+        }
+    }
+}
