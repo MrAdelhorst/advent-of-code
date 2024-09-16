1335773f9466a919e445eb7edc7d9f8a0714dc1c
Kenneth Adelhorst Andersen <kean@demant.com>
2024-09-16 20:13:36 +0200

    2015 - days 1-5

---
+++ 2015/2/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Puzzle 1
+            //var total = File.ReadAllLines(@"c:\data\aoc\2015\2\aoc2-1.txt").Sum(line =>
+            //{
+            //    var sides = line.Split('x').Select(int.Parse).OrderBy(e => e).ToList();
+            //    return sides[0] * sides[1] * 3 + (sides[1] * sides[2] + sides[0] * sides[2]) * 2;
+            //});
+            //Console.WriteLine($"Total wrapping paper: {total}");
+
+            //Puzzle 2
+            var total = File.ReadAllLines(@"c:\data\aoc\2015\2\aoc2-1.txt").Sum(line =>
+            {
+                var sides = line.Split('x').Select(int.Parse).OrderBy(e => e).ToList();
+                return sides[0] * 2 + sides[1] * 2 + sides[0] * sides[1] * sides[2];
+            });
+
+            Console.WriteLine($"Total ribbon: {total}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2015/5/ConsoleApp1/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Puzzle 1
+            //var rules = new Predicate<string>[] { Puzzle1_Rule1, Puzzle1_Rule2, Puzzle1_Rule3 };
+            
+            //Puzzle 2
+            var rules = new Predicate<string>[] { Puzzle2_Rule1, Puzzle2_Rule2 };
+
+            var niceStrings = File.ReadAllLines(@"c:\data\aoc\2015\5\aoc5-1.txt").Count(line => rules.All(rule => rule(line)));
+
+            Console.WriteLine($"Nice strings: {niceStrings}");
+            Console.ReadLine();
+        }
+
+        private static bool Puzzle2_Rule1(string line)
+        {
+            for (int i = 0; i < line.Length - 1; i++)
+                if (line.Substring(i+2, line.Length - i - 2).Contains(line.Substring(i, 2)))
+                    return true;
+
+            return false;
+        }
+        
+        private static bool Puzzle2_Rule2(string line)
+        {
+            for (int i = 0; i < line.Length - 2; i++)
+                if (line[i] == line[i + 2])
+                    return true;
+
+            return false;
+        }
+
+        private static bool Puzzle1_Rule3(string line)
+        {
+            var evilStrings = new string[] { "ab", "cd", "pq", "xy" };
+            foreach (var evil in evilStrings) 
+                if (line.Contains(evil)) return false;
+
+            return true;
+        }
+
+        private static bool Puzzle1_Rule2(string line)
+        {
+            for (int i=0; i<line.Length-1; i++)
+                if (line[i] == line[i+1])
+                    return true;
+
+            return false;
+        }
+
+        private static bool Puzzle1_Rule1(string line)
+        {
+            var vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            return line.Count(vowels.Contains) >= 3;
+        }
+    }
+}
