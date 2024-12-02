6580fc33475f0a7d2d51eefaecdec623d4b954ad
Kenneth A. Andersen <kean@demant.com>
2024-12-02 10:28:23 +0100

    2024, days 1 and 2

---
+++ 2024/01/ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿var pairs = File.ReadAllLines(@"..\..\..\..\day-01.txt")
+    .Select(x => System.Text.RegularExpressions.Regex.Split(x, @"\s+"));
+var list1 = pairs.Select(x => int.Parse(x[0])).Order().ToList();
+var list2 = pairs.Select(x => int.Parse(x[1])).Order().ToList();
+
+var sum = 0;
+var similarityScore = 0;
+
+for (int i=0; i<list1.Count; i++)
+{
+    sum += Math.Abs(list1[i] - list2[i]);
+    similarityScore += list1[i] * list2.Count(x => x == list1[i]);
+}
+
+Console.WriteLine($"Part 1 - Sum: {sum}"); 
+Console.WriteLine($"Part 2 - Score: {similarityScore}");
---
+++ 2024/02/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿var reports = File.ReadAllLines(@"..\..\..\..\day-02.txt")
+    .Select(x => x.Split(' ').Select(int.Parse).ToList());
+
+static bool Valid(List<int> report)
+{
+    bool ascending = report[1] > report[0];
+    return report.GetRange(1, report.Count - 1)
+        .Aggregate(report[0], (prev, current) =>
+        {
+            var diff = current - prev;
+            return Math.Abs(diff) > 0 
+                && Math.Abs(diff) < 4 
+                && diff > 0 == ascending
+                    ? current
+                    : -1000;
+        }) != -1000;
+}
+
+static bool Valid2(List<int> report)
+{
+    return Enumerable.Range(0, report.Count)
+        .Select(idx =>
+        {
+            var tmp = new List<int>(report);
+            tmp.RemoveAt(idx);
+            return tmp;
+        })
+        .Any(Valid);
+}
+
+Console.WriteLine($"Part 1 - Valid reports: {reports.Count(Valid)}");
+Console.WriteLine($"Part 2 - Valid reports: {reports.Count(Valid2)}"); 
