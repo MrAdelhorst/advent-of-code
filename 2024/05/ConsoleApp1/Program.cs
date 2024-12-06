7752109202bb4764ec9a6b83c002574ebbd68145
Kenneth A. Andersen <kean@demant.com>
2024-12-06 17:13:58 +0100

    days 5 and 6

---
+++ 2024/05/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Immutable;
+using System.Text.RegularExpressions;
+
+var lines = File.ReadAllLines(@"..\..\..\..\day-05.txt");
+var updates = lines.Select(line => new Regex(@"^\d+(,\d+)*$").Match(line))
+    .Where(m => m.Success)
+    .Select(x => x.Value.Split(',')
+        .Select(int.Parse)
+        .ToList())
+    .ToList();
+
+var ordering = lines.Select(line => new Regex(@"\d+\|\d+").Match(line))
+    .Where(m => m.Success)
+    .Select(x => x.Value.Split('|'))
+    .Select(x => (key: int.Parse(x[0]), value: int.Parse(x[1])))
+    .ToImmutableHashSet();
+
+var comparer = new PageComparer(ordering);
+var sortedUpdates = updates
+    .Select(update => (original: update, ordered: update.Order(comparer).ToList()))
+    .Select(x => (update: x.ordered, equal: x.original.SequenceEqual(x.ordered)))
+    .ToList();
+
+Console.WriteLine($"Part 1 - {sortedUpdates.Where(x => x.equal).Sum(x => x.update[x.update.Count / 2])}");
+Console.WriteLine($"Part 2 - {sortedUpdates.Where(x => !x.equal).Sum(x => x.update[x.update.Count / 2])}");
+Console.ReadLine();
+
+internal class PageComparer(ImmutableHashSet<(int key, int value)> ordering) : IComparer<int>
+{
+    public int Compare(int x, int y)
+    {
+        if (ordering.Contains((x,y)))
+            return -1;
+
+        if (ordering.Contains((y, x)))
+            return 1;
+
+        return 0;
+    }
+}
---
+++ 2024/06/ConsoleApp1/Program.cs
@@ -0,0 +1,81 @@
+﻿var map = File.ReadAllLines(@"..\..\..\..\day-06.txt")
+    .Select(line => line.ToArray()).ToArray();
+
+//Find the starting position
+var start = new Coordinate(0, 0);
+for (int y = 0; y < map.Length; y++)
+    for (int x = 0; x < map[0].Length; x++)
+        if (map[y][x] == '^')
+            start = new Coordinate(x, y);
+
+//Iterate all possible obstacle positions
+var infiniteLoops = 0;
+for (int y = 0; y < map.Length; y++)
+    for (int x = 0; x < map[0].Length; x++)
+    {
+        var newMap = map.CloneMap();
+        newMap[y][x] = '#';
+        
+        if (NavigateMap(start, newMap).isInfiniteLoop)
+            infiniteLoops++;
+    }
+
+Console.WriteLine($"Part 1 - {NavigateMap(start, map).map.CountSteps()}");
+Console.WriteLine($"Part 2 - {infiniteLoops}");
+Console.ReadLine();
+
+static (char[][] map, bool isInfiniteLoop) NavigateMap(Coordinate start, char[][] map)
+{
+    var maxIterations = map.Length * map[0].Length;
+    var iterations = 0;
+    var current = start;
+    var direction = new Coordinate(0, -1);
+    do
+    {
+        map[current.Y][current.X] = 'X';
+        var newPos = current.Move(direction);
+        if (newPos.InsideMap(map) && map[newPos.Y][newPos.X] == '#')
+        {
+            direction = direction.Turn();
+        }
+        else
+        {
+            current = newPos;
+            iterations++;
+        }
+    } while (current.InsideMap(map) && iterations < maxIterations);
+    return (map, iterations == maxIterations);
+}
+
+record Coordinate(int x, int y)
+{
+    public int X => x;
+    public int Y => y;
+
+    public Coordinate Turn()
+    {
+        return x switch
+        {
+            0 => y switch
+            {
+                -1 => new Coordinate(1, 0),
+                _ => new Coordinate(-1, 0)
+            },
+            1 => new Coordinate(0, 1),
+            _ => new Coordinate(0, -1)
+        };
+    }
+
+    public Coordinate Move(Coordinate direction) => new Coordinate(x + direction.x, y + direction.y);
+    public bool InsideMap(char[][] map) => x >= 0 && x < map[0].Length && y >= 0 && y < map.Length;
+}
+
+public static class Extensions
+{
+    public static char[][] CloneMap(this char[][] map)
+    {
+        return map.Select(x => x.ToArray()).ToArray();
+    }
+
+    public static int CountSteps(this char[][] map) => map.Sum(column => column.Count(x => x == 'X'));
+}
